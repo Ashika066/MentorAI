071a2b5aeb27817b8a33188ad93b620f4d1424c3
Ashika <ashika922005@gmail.com>
2025-03-06 22:07:26 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,46 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "MentorAI - AI-powered mentor for careers",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider appearance={{
+      baseTheme:dark,
+    }}>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={`${inter.className}`}
+      >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/*header*/}
+            <Header/>
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+            {/*footer*/}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-green-200">
+                <p>Made with ♥ by Ashika Gahlyan</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
